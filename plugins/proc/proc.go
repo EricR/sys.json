61b6cbbd2f1b2226bf43e94ca73b32d7335b3bc2
Cole Brumley <cole.brumley@gmail.com>
2015-03-05 00:17:00 -0500

    Adding disk size/free bytes to disk metrics

--- plugins/proc/proc.go
+++ plugins/proc/proc.go
@@ -7,25 +7,6 @@ import (
 	"unicode"
 )
 
-type DiskStats struct {
-	Reads  DiskStatsForOp `json:"reads"`
-	Writes DiskStatsForOp `json:"writes"`
-	IO     DiskStatsForIO `json:"io"`
-}
-
-type DiskStatsForIO struct {
-	InProgress      int `json:"in_progress"`
-	TotalMs         int `json:"total_ms"`
-	TotalWeightedMs int `json:"total_weighted"ms"`
-}
-
-type DiskStatsForOp struct {
-	Completed int `json:"completed"`
-	Sectors   int `json:"sectors"`
-	Merged    int `json:"merged"`
-	TotalMs   int `json:"total_ms"`
-}
-
 type NetworkStats struct {
 	Receieve NetworkStatsSide `json:"receieve"`
 	Send     NetworkStatsSide `json:"send"`
@@ -126,45 +107,6 @@ func GetNetworkInfo() map[string]interface{} {
 					Errors:  util.ParseInt(fields[11]),
 					Dropped: util.ParseInt(fields[12]),
 				},
-			}
-		}
-	})
-
-	return stats
-}
-
-func GetDiskInfo() map[string]interface{} {
-	stats := map[string]interface{}{}
-	partitions := []string{}
-
-	util.EachLine("/proc/partitions", func(fields []string) {
-		if fields[0] != "major" {
-			partitions = append(partitions, fields[3])
-		}
-	})
-
-	util.EachLine("/proc/diskstats", func(fields []string) {
-		for _, partition := range partitions {
-			if fields[2] == partition {
-				stats[partition] = DiskStats{
-					Reads: DiskStatsForOp{
-						Completed: util.ParseInt(fields[3]),
-						Merged:    util.ParseInt(fields[4]),
-						Sectors:   util.ParseInt(fields[5]),
-						TotalMs:   util.ParseInt(fields[6]),
-					},
-					Writes: DiskStatsForOp{
-						Completed: util.ParseInt(fields[7]),
-						Merged:    util.ParseInt(fields[8]),
-						Sectors:   util.ParseInt(fields[9]),
-						TotalMs:   util.ParseInt(fields[10]),
-					},
-					IO: DiskStatsForIO{
-						InProgress:      util.ParseInt(fields[11]),
-						TotalMs:         util.ParseInt(fields[11]),
-						TotalWeightedMs: util.ParseInt(fields[12]),
-					},
-				}
 			}
 		}
 	})
